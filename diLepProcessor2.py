c5fc113bfe944c5d3968b51bea53210a116a6312
zxmei1 <mei1@bu.edu>
2020-08-11 15:11:49 +0800

    aug11 update

--- diLepProcessor2.py
+++ diLepProcessor2.py
@@ -238,7 +238,7 @@ def main():
                                       treename='Events',
                                       processor_instance=exampleProcessor(),
                                       executor=processor.futures_executor,
-                                      executor_args={'workers': 12, 'function_args': {'flatten': False}},
+                                      executor_args={'workers': 24, 'function_args': {'flatten': False}},
                                       chunksize=100000,
                                      )
         cache['fileset']        = fileset
